f1f230e6b90fc2ed171a1dd89772e33f4c3c1eb3
cgonzales20099-cyber <cgonzales20099@iesjoanramis.org>
2026-04-01 13:59:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,35 +5,40 @@ let FPS = 50;
 //PAISATGE
 let ampleC = 50;
 let altC = 50;
+let cesped = '#06d606';
+let terra = '#b66e02';
+let aigua = '#148bcf';
+let pedra = '#8b8a8a';
+let clau = '#eeff00';
+let porta = '#682f00'
+let botasagua = '#b90db1'
 
 let tilemap
 let retras = 0;
-let mostrarInventario = false;
-
 let escenari = [
 
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 4, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 6, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 3, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 8, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 8, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 3, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0, 0, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 3, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
     [0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 0, 0, 0, 2, 0, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
-    [0, 2, 2, 2, 2, 2, 2, 0, 5, 2, 0, 2, 2, 0, 0, 2, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 0, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 2, 0, 0, 0, 2, 0, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
+    [0, 2, 2, 2, 2, 2, 2, 0, 3, 2, 0, 2, 2, 0, 0, 2, 0, 2, 2, 2, 2, 2, 2, 0, 0, 0, 2, 2, 2, 0, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
     [0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 0, 2, 2, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2, 2, 2, 2, 0, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
@@ -41,31 +46,16 @@ let escenari = [
 ]
 function dibuixaEscenari() {
     let color;
-
     for (let y = 0; y < 24; y++) {
         for (let x = 0; x < 48; x++) {
             let tile = escenari[y][x]
-            if (tile <= 3) {
-                ctx.drawImage(tilemap2, tile * 32, 0, 32, 32, x * ampleC, y * altC, ampleC, altC)
-
-            } else if (tile >= 4 && tile <= 7) {
-                ctx.drawImage(tilemap3, (tile - 4) * 32, 0, 32, 32, x * ampleC, y * altC, ampleC, altC)
-            }
-
-            else if (tile >= 8 && tile <= 11) {
-                ctx.drawImage(tilemap4, (tile - 8) * 32, 0, 32, 32, x * ampleC, y * altC, ampleC, altC)
-            }
+            ctx.drawImage(tilemap2,tile*32,0 ,32,32,x*ampleC,y*altC,ampleC,altC)
         }
     }
 }
-
 //PROTAGONISTA Y VILLANO
 let imatge;
 let imatge2;
-let imatge3;
-let tilemap2
-let tilemap3
-let tilemap4
 //INICIALITZA EL JOC NOMES UNA VEGADA
 
 function inicializar() {
@@ -73,16 +63,10 @@ function inicializar() {
     ctx = canvas.getContext('2d');
     imatge = new Image();
     imatge2 = new Image();
-    imatge3 = new Image()
     tilemap2 = new Image();
-    tilemap3 = new Image();
-    tilemap4 = new Image();
     imatge.src = './morda.png'
     imatge2.src = './riri.png'
-    imatge3.src = './ojo.png'
     tilemap2.src = './tilemap2.png'
-    tilemap3.src = './llaves123.png'
-    tilemap4.src = './puertas.png'
     //INTERVAL QUE S'EXECUTA 50 VEGADAS PER SEGON
     setInterval(function () {
         principal();
@@ -97,161 +81,56 @@ let prota = function (x, y, vel) {
     this.x = x;
     this.y = y;
     this.clau = false;
-    this.inventario = []
+    
 
-    this.agregarItem = function (item) {
-        this.inventario.push(item)
-        console.log("Has conseguido: " + item)
-    }
-    this.mostrarInventario = function () {
-        console.log("Inventario");
-        for (let i = 0; i < this.inventario.length; i++) {
-            console.log("- " + this.inventario[i]);
-        }
+   
 
-    }
+   
+   
+   
+    
+    this.muerte = function(x,y){
 
-    this.incluyeItem = function (item) {
-        return this.inventario.includes(item)
-
-    }
-
-    this.muerte = function (x, y) {
-
-        let died = false;
-        if (this.x == x && this.y == y) {
+    let died = false;
+        if(this.x == x && this.y == y){
             died = true;
         }
         return died
     }
 
 
-    this.dibuixa = function () {
-        ctx.drawImage(imatge, this.x, this.y, 50, 50)
+    this.dibuixa = function() {
+        ctx.drawImage(imatge, this.x, this.y, 50 , 50)
     }
     this.texte = function () {
         ctx.font = '30px impact'
         ctx.fillStyle = '#555555'
-        ctx.fillText("X: " + this.x + " Y: " + this.y, 50, 50)
+        ctx.fillText("X: " + this.x + " Y: " + this.y, 50 , 50)
     }
     this.margenes = function (x, y) {
         let colisio = false;
-        if (escenari[y / 50][x / 50] == 0) {
+        if (escenari[y / 50][x / 50] == 0 || escenari[y / 50][x / 50] == 1 ) {
             colisio = true;
 
             return colisio;
         }
-
-        if (escenari[y / 50][x / 50] == 1 && !this.incluyeItem("Llave")) {
-            return true;
-        }
-        if (escenari[y / 50][x / 50] == 8 && !this.incluyeItem("Llave clasica")) {
-            return true;
-        }
-        if (escenari[y / 50][x / 50] == 9 && !this.incluyeItem("Llave azul")) {
-            return true;
-        }
-        if (escenari[y / 50][x / 50] == 10 && !this.incluyeItem("Llave verde")) {
-            return true;
-        }
-        if (escenari[y / 50][x / 50] == 11 && !this.incluyeItem("Llave roja")) {
-            return true;
-        }
-        return false;
+        return colisio;
     }
-
     this.logica = function () {
         if (escenari[this.y / 50][this.x / 50] == 3) {
-
+            
             alert("Has encontrado una llave de oro")
             escenari[this.y / 50][this.x / 50] = 2
-            this.agregarItem("Llave");
         }
         if (escenari[this.y / 50][this.x / 50] == 1) {
-            if (this.incluyeItem("Llave")) {
-
-                console.log("eres el puto amo, has salido vivo")
-
-            } else {
-
-                alert("esta cerrado tontorron")
-
-            }
-        }
-    }
-    this.logica2 = function () {
-        if (escenari[this.y / 50][this.x / 50] == 4) {
-
-            alert("Has encontrado una Llave clasica")
-            escenari[this.y / 50][this.x / 50] = 2
-            this.agregarItem("Llave clasica");
-        }
-        if (escenari[this.y / 50][this.x / 50] == 8) {
-            if (this.incluyeItem("Llave clasica")) {
-
-                console.log("eres el puto amo, has salido vivo")
-
-            } else {
-
-                alert("esta cerrado tontorron")
-
-            }
-        }
-    }
-    this.logica3 = function () {
-        if (escenari[this.y / 50][this.x / 50] == 5) {
-
-            alert("Has encontrado una llave azul")
-            escenari[this.y / 50][this.x / 50] = 2
-            this.agregarItem("Llave azul");
-        }
-        if (escenari[this.y / 50][this.x / 50] == 9) {
-            if (this.incluyeItem("Llave azul")) {
-
-                console.log("eres el puto amo, has salido vivo")
-
-            } else {
-
-                alert("esta cerrado tontorron")
-
-            }
-        }
-    }
-    this.logica4 = function () {
-        if (escenari[this.y / 50][this.x / 50] == 6) {
-
-            alert("Has encontrado una llave roja")
-            escenari[this.y / 50][this.x / 50] = 2
-            this.agregarItem("Llave verde");
-        }
-        if (escenari[this.y / 50][this.x / 50] == 10) {
-            if (this.incluyeItem("Llave verde")) {
-
-                console.log("eres el puto amo, has salido vivo")
-
-            } else {
-
-                alert("esta cerrado tontorron")
-
-            }
-        }
-    }
-    this.logica5 = function () {
-        if (escenari[this.y / 50][this.x / 50] == 7) {
-
-            alert("Has encontrado una llave roja")
-            escenari[this.y / 50][this.x / 50] = 2
-            this.agregarItem("Llave clasica");
-        }
-        if (escenari[this.y / 50][this.x / 50] == 11) {
-            if (this.incluyeItem("Llave roja")) {
-
-                console.log("eres el puto amo, has salido vivo")
-
-            } else {
-
-                alert("esta cerrado rosa")
-
+            if (this.clau) {
+            
+                alert("eres el puto amo, has salido vivo")
+             
+            } else  {
+              
+                alert("esta cerrado tontorron") 
+         
             }
         }
     }
@@ -259,47 +138,35 @@ let prota = function (x, y, vel) {
 
 
 
-    this.adalt = function () {
-        if (!this.margenes(this.x, this.y - 50)) {
-            this.y = this.y - 50
-        }
-        this.logica()
-        this.logica2()
-        this.logica3()
-        this.logica4()
-        this.logica5()
+this.adalt = function () {
+    if (!this.margenes(this.x, this.y - 50)) {
+        this.y = this.y - 50
     }
+    this.logica()
+    
+}
 
-    this.baix = function () {
-        if (!this.margenes(this.x, this.y + 50)) {
-            this.y = this.y + 50
-        }
-        this.logica()
-        this.logica2()
-        this.logica3()
-        this.logica4()
-        this.logica5()
+this.baix = function () {
+    if (!this.margenes(this.x, this.y + 50)) {
+        this.y = this.y + 50
     }
-    this.dreta = function () {
-        if (!this.margenes(this.x + 50, this.y)) {
-            this.x = this.x + 50
-        }
-        this.logica()
-        this.logica2()
-        this.logica3()
-        this.logica4()
-        this.logica5()
+    this.logica()
+    
+}
+this.dreta = function () {
+    if (!this.margenes(this.x + 50 , this.y)) {
+        this.x = this.x + 50
     }
-    this.esquerra = function () {
-        if (!this.margenes(this.x - 50, this.y)) {
-            this.x = this.x - 50
-        }
-        this.logica()
-        this.logica2()
-        this.logica3()
-        this.logica4()
-        this.logica5()
+    this.logica()
+    
+}
+this.esquerra = function () {
+    if (!this.margenes(this.x - 50 , this.y)) {
+        this.x = this.x - 50
     }
+    this.logica()
+    
+}
 }
 
 let enemics = function (x, y, vel) {
@@ -309,12 +176,12 @@ let enemics = function (x, y, vel) {
 
     this.dibuixa = function () {
 
-        ctx.drawImage(imatge2, this.x, this.y, 50, 50)
+        ctx.drawImage(imatge2,this.x, this.y, 50 , 50)
 
     }
     this.margenes = function (x, y) {
         let colisio = false;
-        if (escenari[y / 50][x / 50] == 0 ) {
+        if (escenari[y / 50][x / 50] == 0 || escenari[y / 50][x / 50] == 1) {
             colisio = true;
 
             return colisio;
@@ -323,23 +190,22 @@ let enemics = function (x, y, vel) {
     }
     this.mou = function () {
         let haMuerto = sophie.muerte(this.x, this.y)
-        if (haMuerto) {
+        if(haMuerto){
             alert("HAS MUERTO BOBO")
             sophie.x = 800
-            sophie.y = 600
+            sophie.Y = 600 
         }
         retras++;
         if (retras == 50) {
-            retras = 0
-          
+            retras=0
             let posicio = Math.floor(Math.random() * 4)
-            
+
             if (posicio == 0) {
-                if (!this.margenes(this.x - 50, this.y))
+                if (!this.margenes(this.x - 50 , this.y))
                     this.x = this.x - 50
             }
             if (posicio == 1) {
-                if (!this.margenes(this.x + 50, this.y))
+                if (!this.margenes(this.x + 50 , this.y))
                     this.x = this.x + 50
             }
             if (posicio == 2) {
@@ -350,29 +216,19 @@ let enemics = function (x, y, vel) {
                 if (!this.margenes(this.x, this.y + 50))
                     this.y = this.y + 50
             }
-            
         }
-
+    
     }
 }
 
-
-
-
-
-
-
-let sophie = new prota(200, 550);
-let porqueria = new enemics(200, 300)
-let gay = new enemics(200, 400)
-let Omar = new enemics(600, 1000)
+let sophie = new prota(200 , 550);
+let porqueria = new enemics(200 , 300 , 10)
+let gay = new enemics (200,400,10)
+let Omar = new enemics (200,500,10)
 
 
 
 document.addEventListener('keydown', function (tecla) {
-    if (tecla.key == 'i') {
-        mostrarInventario = !mostrarInventario
-    }
     if (tecla.key == 'ArrowUp') {
         sophie.adalt()
 
@@ -392,28 +248,10 @@ document.addEventListener('keydown', function (tecla) {
 
     }
 })
-function dibujarInventario() {
-
-
-    ctx.fillStyle = "rgba(0,0,0,0.7)";
-    ctx.fillRect(500, 200, 800, 600);
-
-
-    ctx.fillStyle = "white";
-    ctx.font = "50px Arial";
-    ctx.fillText("Inventario", 550, 260);
-
-
-    ctx.font = "40px Arial";
-
-    for (let i = 0; i < sophie.inventario.length; i++) {
-        ctx.fillText("- " + sophie.inventario[i], 550, 320 + i * 40);
-    }
-}
 
 function principal() {
     borrarPantalla();
-    dibuixaEscenari();
+    dibuixaEscenari()
 
     sophie.dibuixa();
     porqueria.dibuixa();
@@ -422,13 +260,7 @@ function principal() {
     gay.mou()
     Omar.dibuixa()
     Omar.mou()
-    david.dibuixa()
-    david.mou()
-    david2.dibuixa()
-    david2.mou()
-    if (mostrarInventario) {
-        dibujarInventario();
-    }
+    
 
 }
 
